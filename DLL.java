6278cdf8fc19663c69dd79155e90fe79d5023642
HenriettaEdmonds <he85410n@pace.edu>
2026-02-17 16:00:41 -0500

    Update DLL.java

--- DLL.java
+++ DLL.java
@@ -50,7 +50,98 @@ class DLL<E> {
     }
 
 
+// DLL Feilds
+    private Node<E> head;
+    private Node<E> tail;
+    private int size;
 
+    // Constructor
+    public DLL() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    // int size()
+    public int size() {
+        return size;
+    }
+
+    // boolean.isEmpty()
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // E first
+    public E first() {
+        if (isEmpty())
+            return null;
+        return head.getElement();
+    }
+
+    // E last
+    public E last() {
+        if (isEmpty())
+            return null;
+        return tail.getElement();
+    }
+
+    // void addFrist
+    public void addFirst(E element) {
+        Node<E> newNode = new Node<>(element);
+
+        if (isEmpty()) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.setNext(head);
+            head.setPrev(newNode);
+            head = newNode;
+        }
+
+        size++;
+    }
+
+    // E removeLast
+    public E removeLast() {
+        if (isEmpty())
+            return null;
+        E removedElement = tail.getElement(); 
+
+        if (head == tail) { 
+            head = null;
+            tail = null;
+        } else { 
+            Node<E> prevNode = tail.getPrev();
+            prevNode.setNext(null);
+            tail = prevNode;
+        }
+
+        size--;
+        return removedElement;
+
+    }
+
+    // E removeFirst
+    public E removeFirst() {
+        if (isEmpty())
+            return null; /
+
+        E removedElement = head.getElement(); 
+
+        if (head == tail) { 
+            head = null;
+            tail = null;
+        } else { 
+            Node<E> nextNode = head.getNext();
+            nextNode.setPrev(null);
+            head = nextNode;
+        }
+
+        size--;
+        return removedElement;
+
+    }
 
 
 
